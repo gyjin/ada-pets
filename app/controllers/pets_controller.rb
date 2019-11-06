303299281ef984ff67a178a11815fca139003176
Ga-Young Jin <gyjinn@gmail.com>
2019-11-06 10:35:36 -0800

    readded index to make tests pass

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,13 +1,13 @@
 class PetsController < ApplicationController
-  # def index
-  #   pets = Pet.all
-  
-  #   # below code returns only the keys we specify 
-  #   render :json => pets.as_json(only: [:id, :name, :age, :human]), status: :ok
-  
-  #   # below code returns all variables of each object
-  #   # render json: pets, status: :ok
-  # end
+  def index
+    pets = Pet.all
+    
+    # below code returns only the keys we specify 
+    render :json => pets.as_json(only: [:id, :name, :age, :human]), status: :ok
+    
+    # below code returns all variables of each object
+    # render json: pets, status: :ok
+  end
   
   def show
     pet = Pet.find_by_id(params[:id])
