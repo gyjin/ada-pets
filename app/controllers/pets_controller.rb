7f16ec6819b431d4e67c98ef1764ab777217341b
Ga-Young Jin <gyjinn@gmail.com>
2019-11-06 10:25:52 -0800

    added travis yml file

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,13 +1,28 @@
 class PetsController < ApplicationController
   def index
-    @pets = Pet.all
+    pets = Pet.all
+    
+    # below code returns only the keys we specify 
+    render :json => pets.as_json(only: [:id, :name, :age, :human]), status: :ok
+    
+    # below code returns all variables of each object
+    # render json: pets, status: :ok
   end
-
-
-
-  private
-
-    def pet_params
-      params.require(:pet).permit(:name, :age, :human)
+  
+  def show
+    pet = Pet.find_by_id(params[:id])
+    
+    if pet.nil?
+      render json: {"errors" => "no pet by that ID"}, status: :not_found
+      return
     end
+    
+    render :json => pet.as_json(only: [:id, :name, :age, :human]), status: :ok
+  end
+  
+  private
+  
+  def pet_params
+    params.require(:pet).permit(:name, :age, :human)
+  end
 end
